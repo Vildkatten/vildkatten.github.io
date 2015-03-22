afc31b310331c1c18a75d61e2a508ed5270c0ef7
Emil Jönsson <vildkatten@Vildkattens-MacBook-Pro-4.local>
2015-03-22 22:39:33 +0100

    add time on menu bar

--- js/script.js
+++ js/script.js
@@ -7,17 +7,46 @@
     	$(window).scrollTo( 0, 500);
 });
 
-    	var currentDate = new Date();
-    	var month = currentDate.getMonth() + 1;
-    	var day = currentDate.getDate();
-    	var year = currentDate.getFullYear();
+    	function startDate(){
+    		var time = new Date();
+    		var month = time.getMonth() + 1;
+	    	var day = time.getDate();
+	    	var year = time.getFullYear();
+	    	month = addZero(month);
+    		day = addZero(day);
 
-    	if (month < 10){
-    		month = "0" + month;
+    		document.getElementById('date').innerHTML = year + "-" + month + "-" + day;
     	}
 
-    	document.getElementById('date').innerHTML = year + "-" + month + "-" + day;
+    	 	startDate();
+
+	    function startTime(){
+	    	var time = new Date();
+	    	var hour = time.getHours();
+    		var minutes = time.getMinutes();
+    		var seconds = time.getSeconds();
+    		minutes = addZero(minutes);
+    		seconds = addZero(seconds);
+
+    		document.getElementById('time').innerHTML = hour + ":" + minutes + ":" + seconds;
+
+    		var t = setTimeout(function(){
+    			startTime();
+    		},500);
+
+	    }
+
+	    	startTime();
 
 
+    	function addZero(i) {
+    		if (i < 10){
+    			i = "0" + i;
+    			return i;
+    		}else{
+    			i = i;
+    			return i;
+    		}
+    	}
 
     });
