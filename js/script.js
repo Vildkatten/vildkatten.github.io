262c37c160ec8bffae3e844aac9da784c5e282ee
Emil Jönsson <vildkatten@Vildkattens-MacBook-Pro-4.local>
2015-03-22 21:24:02 +0100

    add date on menu bar

--- js/script.js
+++ js/script.js
@@ -6,5 +6,18 @@
     	$("#up").click(function(event) {
     	$(window).scrollTo( 0, 500);
 });
+
+    	var currentDate = new Date();
+    	var month = currentDate.getMonth() + 1;
+    	var day = currentDate.getDate();
+    	var year = currentDate.getFullYear();
+
+    	if (month < 10){
+    		month = "0" + month;
+    	}
+
+    	document.getElementById('date').innerHTML = year + "-" + month + "-" + day;
+
+
+
     });
-    
